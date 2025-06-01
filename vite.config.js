5b058215ad78e1c631bcae63ec1787eb9ac9f1c3
WigglesVz <cristopherr1103@gmail.com>
2025-05-31 20:29:50 -0400

    Update vite.config.js to optimize Dexie dependency

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // No se necesitan plugins adicionales para Vite por defecto para este problema.
-  // Las configuraciones de build y optimizeDeps son las áreas a considerar.
-  // Por ahora, empecemos con un config casi vacío para ver si solo tenerlo
-  // ayuda a Vite a hacer un análisis más profundo o si necesitamos añadir algo.
-  build: {
-    // Opciones de build si fueran necesarias más adelante
-  },
-  // optimizeDeps: { // Esto es principalmente para el servidor de desarrollo,
-                  // pero a veces puede influir en cómo se tratan las dependencias.
-  //   include: ['dexie'],
+  optimizeDeps: {
+    include: ['dexie'],
+  }
+  // Puedes añadir la sección 'build' si es necesario más adelante
+  // build: {
+  //   rollupOptions: {
+  //     // ...
+  //   }
   // }
 });
